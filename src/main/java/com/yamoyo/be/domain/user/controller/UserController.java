a4b7c091236a1066a5140ee4f790975e690a9269
bell <bellhyunforgithub@gmail.com>
2026-01-25 01:59:26 +0900

    Fix UserControllerTest failures due to missing config, validation handling, and security redirection

---
+++ src/main/java/com/yamoyo/be/domain/user/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.yamoyo.be.domain.user.controller;
+
+import com.yamoyo.be.common.dto.ApiResponse;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController("/api/users")
+public class UserController {
+
+
+
+}
