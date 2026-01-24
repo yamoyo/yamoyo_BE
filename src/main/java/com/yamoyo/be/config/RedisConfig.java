88fad8053345e41ec28fc077c888a410eb27cd07
woojeans7 <thddnwls71@naver.com>
2026-01-24 23:53:20 +0900

    Feat : Redis 설정 추가

---
+++ src/main/java/com/yamoyo/be/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.yamoyo.be.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    /**
+     * RedisTemplate 설정
+     * - Key: String (초대링크 토큰)
+     * - Value: JSON (팀룸 ID)
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // Key Serializer: String
+        template.setKeySerializer(new StringRedisSerializer());
+
+        // Value Serializer: JSON
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return template;
+    }
+}
