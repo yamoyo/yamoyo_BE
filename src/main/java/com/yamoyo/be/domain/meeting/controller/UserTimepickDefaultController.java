2588200f43cdae6cc356d06ebb5c8548f92cece6
sangyeobhan <sangyeobhandev@gmail.com>
2026-01-27 15:59:02 +0900

    refactor: JwtTokenClaims를 활용하도록 @AuthenticationPrincipal 변경 및 관련 코드 수정

--- src/main/java/com/yamoyo/be/domain/meeting/controller/UserTimepickDefaultController.java
+++ src/main/java/com/yamoyo/be/domain/meeting/controller/UserTimepickDefaultController.java
@@ -5,8 +5,8 @@ import com.yamoyo.be.domain.meeting.dto.response.AvailabilityResponse;
 import com.yamoyo.be.domain.meeting.service.UserTimepickDefaultService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import com.yamoyo.be.domain.security.jwt.JwtTokenClaims;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +21,9 @@ public class UserTimepickDefaultController {
 
     @GetMapping("/availability")
     public ApiResponse<AvailabilityResponse> getAvailability(
-            @AuthenticationPrincipal OAuth2User oAuth2User
+            @AuthenticationPrincipal JwtTokenClaims claims
     ) {
-        Long userId = (Long) oAuth2User.getAttributes().get("userId");
+        Long userId = claims.userId();
         log.info("가용시간 기본값 조회 요청 - UserId: {}", userId);
 
         AvailabilityResponse response = userTimepickDefaultService.getAvailability(userId);
