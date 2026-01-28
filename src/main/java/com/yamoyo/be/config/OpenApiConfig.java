eca6c33e0b081866150daf1009b4ba73470070f5
sangyeobhan <sangyeobhandev@gmail.com>
2026-01-28 13:05:38 +0900

    fix: Swagger 설정에 LocalTime 직렬화 스키마 추가

--- src/main/java/com/yamoyo/be/config/OpenApiConfig.java
+++ src/main/java/com/yamoyo/be/config/OpenApiConfig.java
@@ -3,18 +3,31 @@ package com.yamoyo.be.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.utils.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalTime;
 import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     private static final String SECURITY_SCHEME_NAME = "Bearer Authentication";
+
+    static {
+        SpringDocUtils.getConfig().replaceWithSchema(
+                LocalTime.class,
+                new Schema<LocalTime>()
+                        .type("string")
+                        .format("time")
+                        .example("14:30:00")
+        );
+    }
 
     @Bean
     public OpenAPI openAPI() {
